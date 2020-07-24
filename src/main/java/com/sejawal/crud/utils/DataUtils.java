4721a2e96b150a23b2662cb15ea4c2e3a23278bb
Bhesh Sejawal <bsejawal@Bheshs-MacBook-Pro.local>
2020-07-24 17:53:01 -0500

    changes for file path error

--- src/main/java/com/sejawal/crud/utils/DataUtils.java
+++ src/main/java/com/sejawal/crud/utils/DataUtils.java
@@ -1,11 +1,9 @@
 package com.sejawal.crud.utils;
 
-import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.DeserializationFeature;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.sejawal.crud.model.Person;
 import com.sejawal.crud.vo.PersonList;
-import org.aspectj.weaver.ast.Test;
 
 import java.io.File;
 import java.io.IOException;
@@ -16,13 +14,21 @@ public class DataUtils {
     public static List<Person> persons(){
         ObjectMapper mapper = new ObjectMapper();
         mapper.disable(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES);
+                PersonList personList = null;
 
-        PersonList personList = null;
-        try {
-            personList =  mapper.readValue(new File("src/main/resources/data/person.json"), PersonList.class);
-        } catch (IOException e) {
+        ClassLoader loader = Thread.currentThread().getContextClassLoader();
+        try(InputStream input = loader.getResourceAsStream("data"+File.separator+"person.json")){
+            personList = mapper.readValue(input, PersonList.class);
+
+        }catch (IOException e){
             e.printStackTrace();
         }
+
+//        try {
+//            personList =  mapper.readValue(new File("src/main/resources/data/person.json"), PersonList.class);
+//        } catch (IOException e) {
+//            e.printStackTrace();
+//        }
         return personList.getPersonList();
 
 
