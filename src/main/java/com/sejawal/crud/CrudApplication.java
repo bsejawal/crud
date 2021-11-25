e7bd4f652825fa18d21229a903c0acab70876c6d
Bhesh Sejawal <bsejawal@gmail.com>
2021-11-25 15:01:40 -0600

    remove unwanted files

--- src/main/java/com/sejawal/crud/CrudApplication.java
+++ src/main/java/com/sejawal/crud/CrudApplication.java
@@ -1,17 +1,7 @@
 package com.sejawal.crud;
 
-import com.sejawal.crud.model.User;
-import com.sejawal.crud.repository.UserRepository;
-import com.sejawal.crud.utils.DataUtils;
-import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Bean;
-import org.springframework.web.servlet.config.annotation.CorsRegistry;
-import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
-
-import java.util.List;
-import java.util.Set;
 
 @SpringBootApplication
 public class CrudApplication {
@@ -19,24 +9,5 @@ public class CrudApplication {
 	public static void main(String[] args) {
 
 		SpringApplication.run(CrudApplication.class, args);
-	}
-
-	@Bean
-	CommandLineRunner init (UserRepository userRepository){
-		return args -> {
-			Set<User> users =DataUtils.users();
-			users.forEach(user -> userRepository.save(user));
-		};
-	}
-
-	@Bean
-	public WebMvcConfigurer corsConfigurer() {
-		return new WebMvcConfigurer() {
-			@Override
-			public void addCorsMappings(CorsRegistry registry) {
-				System.out.println("CrudApplication.addCorsMappings");
-				registry.addMapping("/**").allowedOrigins("http://localhost:3000");
-			}
-		};
 	}
 }
