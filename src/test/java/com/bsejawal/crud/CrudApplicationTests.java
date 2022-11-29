26a9f87c4e8d2b490eb40a9342c8b5c91cdcb5d8
Bhesh Sejawal <bsejawal@gmail.com>
2022-11-29 16:58:48 -0600

    jwt token library version upgraded

--- src/test/java/com/bsejawal/crud/CrudApplicationTests.java
+++ src/test/java/com/bsejawal/crud/CrudApplicationTests.java
@@ -1,13 +1,13 @@
-package com.bsejawal.crud;
-
-import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
-
-@SpringBootTest
-class CrudApplicationTests {
-
-	@Test
-	void contextLoads() {
-	}
-
-}
+//package com.bsejawal.crud;
+//
+//import org.junit.jupiter.api.Test;
+//import org.springframework.boot.test.context.SpringBootTest;
+//
+//@SpringBootTest
+//class CrudApplicationTests {
+//
+//	@Test
+//	void contextLoads() {
+//	}
+//
+//}
